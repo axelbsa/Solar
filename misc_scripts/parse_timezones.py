30e9d39bef121dec51215aefa16a2ce58fd8a0b6
Axel Sanner <axel@xeneta.com>
2017-09-22 21:19:23 +0200

    starting db boilerplate

--- misc_scripts/parse_timezones.py
+++ misc_scripts/parse_timezones.py
@@ -2,10 +2,25 @@ import re
 import chardet
 import codecs
 
+import psycopg2
 
-def parse_countries():
+
+insert_timezone = """
+    INSERT INTO world (
+        country_code, timezone_id, gmt_offset_winter,
+        dst_offset_summer, rawoffset
+    )
+    VALUES ({:s}, {:s}, {:f}, {:f}, {:f})
+"""
+
+
+def db_connect():
+    conn = psycopg2.connect(dbname="world", user="axelbs")
+    return conn
+
+
+def parse_countries(db_conn):
     with codecs.open("timeZones.txt", encoding="utf-8") as f:
-        print f.readline()
         for line in f:
             place = re.split("\t", line.strip())
             if place[6] == 'P' and place[8] == 'NO':
@@ -13,12 +28,20 @@ def parse_countries():
                 # print ' '.join([l.decode("utf-8") for l in place])
 
 
-def parse_timezones():
+def parse_timezones(db_conn):
+    cur = db_conn.cursor()
+    print cur
     with codecs.open("timeZones.txt", encoding="utf-8") as f:
         print f.readline()
         for line in f:
-            print re.split("\t", f.readline().strip())
+            tz_info = re.split("\t", f.readline().strip())
+            cur.execute(insert_timezone.format
+                        (
+                            tz_info[0], tz_info[1], tz_info[2],
+                            tz_info[3], tz_info[4]
+                        ))
+    cur.commit()
 
 
 if __name__ == "__main__":
-    parse_timezones()
+    parse_timezones(db_connect())
